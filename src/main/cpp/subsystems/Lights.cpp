379f7be3020e8a4b18b7191195f7bb51533646f6
gearheads4 <FIRSTTeam102@gmail.com>
2020-02-03 20:00:29 -0500

    Created lights subsystem

---
+++ src/main/cpp/subsystems/Lights.cpp
@@ -0,0 +1,13 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "subsystems/Lights.h"
+
+Lights::Lights() {}
+
+// This method will be called once per scheduler run
+void Lights::Periodic() {}
